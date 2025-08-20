334c4737ef0367c10aa1132a2a4275c1e38a1848
vilho <vilho.s.pesonen@gmail.com>
2025-08-20 13:32:16 +0300

    ohjelma tehty

---
+++ HeiMaailma.py
@@ -0,0 +1,7 @@
+kanta = float(input("anna suorakaiteen kanta:  "))
+korkeus = float(input("Anna suorakaiteen korkeus: "))
+
+ala = kanta * korkeus
+
+#print("pinta-ala on "+str(ala))
+print(f"pinta-ala  on {ala:.2f}")
