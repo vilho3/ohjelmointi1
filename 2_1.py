7a24df7c23ee42ebc5a186d75a83ac045d924171
vilho <vilho.s.pesonen@gmail.com>
2025-08-20 13:54:30 +0300

    ohjelma tehty

---
+++ 2_1.py
@@ -0,0 +1,7 @@
+#Kirjoita ohjelma, joka kysyy nimesi ja sen jälkeen tervehtii sinua omalla nimelläsi.
+# Esimerkkejä:
+#Jos syötät nimeksesi Viivi, ohjelma tervehtii sinua sanoin Terve, Viivi!
+#Jos syötät nimeksesi Ahmed, ohjelma tervehtii sinua sanoin Terve, Ahmed!
+
+käyttäjä = input("Anna nimesi: ")
+print("Terve, "+käyttäjä+"!")
