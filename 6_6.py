c9e54c69c73d244bb816b8f97b70843c713b2c7f
vilho <vilho.s.pesonen@gmail.com>
2025-08-31 00:08:32 +0300

    valmis

---
+++ 6_6.py
@@ -0,0 +1,28 @@
+#Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä
+# sekä pizzan hinnan euroina. Funktio laskee ja palauttaa pizzan yksikköhinnan
+# euroina per neliömetri. Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat
+# ja hinnat sekä ilmoittaa, kumpi pizza antaa paremman vastineen rahalle
+# (eli kummalla on alhaisempi yksikköhinta). Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+import math
+def pizza(halkaisija,hinta_euro):
+    halkaisija=halkaisija/100
+    halkaisija=math.pi*(halkaisija/2)**2
+    halkaisija = hinta_euro/halkaisija
+    print(f"{halkaisija} €/m^2")
+    return halkaisija
+
+
+if __name__ == '__main__':
+    halkaisija1 = float(input("Anna ensimmäisen pizzan halkaisija cm: "))
+    hinta1 = float(input("Anna ensimmäisen pizzan hinta euroina: "))
+    halkaisija2 = float(input("Anna toisen pizzan halkaisija cm: "))
+    hinta2 =float(input("Anna toisen pizzan hinta euroina: "))
+    vastine = pizza(halkaisija1,hinta1)
+    vastine2 = pizza(halkaisija2,hinta2)
+    if vastine >vastine2:
+        print("Ensimmäinen pizza on parempi vastine rahalle! ")
+    elif vastine < vastine2:
+        print("Toinen pizza on parempi vastine rahalle! ")
+    else:
+        print("Molemmat pizzat ovat yhtä hyvä vastine rahalle! ")
+
