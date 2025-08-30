b51cc37a0a59c91408ef3ea7e9e3fced24a13921
vilho <vilho.s.pesonen@gmail.com>
2025-08-31 00:19:01 +0300

    valmis

--- 7_1.py
+++ 7_1.py
@@ -4,8 +4,7 @@
 # Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
 
 
-kuukaudet= ("kevät","kesä","syksy","talvi")
-arvot = 1, (2, 3), 4
+kuukaudet= ("talvi","talvi","kevät","kevät","kevät","kesä","kesä","kesä","syksy","syksy","syksy","talvi")
 kuukausi = int(input("Anna kuukauden numero 1-12: "))
 tulostus = kuukaudet[kuukausi-1]
 print("vuoden aika on: " + (tulostus))
